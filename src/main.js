faef1640433351af5102765b99aeb8ef9814f4fb
Andre Madarang <andre_madarang@hotmail.com>
2021-12-19 15:39:47 -0500

    Working example

--- src/main.js
+++ src/main.js
@@ -1,6 +1,39 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
-createApp(App).use(store).use(router).mount('#app')
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client/core'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://lc-laravel-graphql.test/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+// createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
+  .use(store)
+  .use(router)
+  .mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,131 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Queries</h2>
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">{{ error.message }}</div>
+    <div v-else>
+      <ul v-if="result">
+        <li v-for="post in posts.data" :key="post.id">
+          {{ post.title }}
+        </li>
+      </ul>
+    </div>
+
+    <h2>Query Variables</h2>
+    <div v-if="postResult">{{ postResult.post.title }}</div>
+
+    <h2>Mutations</h2>
+    <div>{{ createPostError }}</div>
+    <form action="#" @submit.prevent="createPostMethod">
+      <div>
+        <label for="title">Title</label>
+        <input v-model="title" type="text" id="title" />
+      </div>
+      <div>
+        <label for="body">Body</label>
+        <textarea
+          v-model="body"
+          name="body"
+          id="body"
+          cols="30"
+          rows="10"
+        ></textarea>
+      </div>
+      <div>
+        <button>Create Post</button>
+        <div v-if="createPostLoading">Loading...</div>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { useMutation, useQuery, useResult } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+import { ref, watchEffect } from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    const title = ref('')
+    const body = ref('')
+    const createPostError = ref('')
+
+    const { result, loading, error } = useQuery(gql`
+      query getPosts {
+        posts {
+          data {
+            id
+            title
+          }
+        }
+      }
+    `)
+
+    const posts = useResult(result, null, data => data.posts)
+
+    const { result: postResult } = useQuery(
+      gql`
+        query getPost($id: ID!) {
+          post(id: $id) {
+            title
+            body
+          }
+        }
+      `,
+      {
+        id: 1,
+      }
+    )
+
+    // watchEffect(() => console.log(result.value))
+
+    const {
+      mutate: createPost,
+      loading: createPostLoading,
+      onError,
+      onDone,
+    } = useMutation(gql`
+      mutation createPost($user_id: ID!, $title: String!, $body: String!) {
+        createPost(user_id: $user_id, title: $title, body: $body) {
+          id
+          title
+        }
+      }
+    `)
+
+    onError(error => {
+      const key = Object.keys(error.graphQLErrors[0].extensions.validation)[0]
+      createPostError.value =
+        error.graphQLErrors[0].extensions.validation[key][0]
+    })
+
+    onDone(result => {
+      title.value = ''
+      body.value = ''
+      createPostError.value = ''
+    })
+
+    function createPostMethod() {
+      createPost({
+        user_id: 1,
+        title: title.value,
+        body: body.value,
+      })
+    }
+
+    return {
+      title,
+      body,
+      createPostError,
+      result,
+      posts,
+      loading,
+      error,
+      postResult,
+      createPostMethod,
+      createPostLoading,
+    }
+  },
 }
 </script>
